50eb75cef8b7de9b34f33a7d6a07867719901c55
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2020-12-22 16:38:02 +0100

    more hyperparameter tuning; implemented different way to insert whitespaces

--- core/license_plate_recognizer.py
+++ core/license_plate_recognizer.py
@@ -5,10 +5,10 @@ import tensorflow as tf
 
 from core.data_prep import get_keys
 
-def extract_letters(dilation, gray, thresh):
+def extract_letters(gray, thresh):
     letter_rects = []
     # find contours of regions of interest within license plate
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # create copy of gray license plate
     im2 = gray.copy()
     height, width = im2.shape
@@ -27,7 +27,7 @@ def extract_letters(dilation, gray, thresh):
         if w < h * 0.25:
             continue
         # if width is not wide enough relative to total width then skip
-        if width / w > 20:
+        if width / w > 25:
             continue
         keep_cntrs.append(cntr)
 
@@ -54,21 +54,20 @@ def extract_letters(dilation, gray, thresh):
     # sort contours left-to-right
     contours = sorted(contours, key=lambda cntr: cv2.boundingRect(cntr)[0]) # TODO: sort from upper left to lower right instead
 
-    whitespace = -1
+    whitespaces = [-1, -1]
 
     for i, cntr in enumerate(contours):
 
         x, y, w, h = cv2.boundingRect(cntr)
         if i == 0:
-            dist, dist_max = 0, 0
+            dist = -float('inf')
+            distances = []
             urc = x + w
         else:
             dist = x - urc
             urc = x + w
 
-        if dist > dist_max:
-            dist_max = dist
-            whitespace = i
+        distances.append((dist, i))
 
         # grab character region of image
         roi = thresh[max(y - 5 - int(h * 0.13), 0):min(y + h + 5, height), max(x - 5, 0):min(x + w + 5, width)]  # some margin for error for umlauts
@@ -78,17 +77,23 @@ def extract_letters(dilation, gray, thresh):
         roi = cv2.medianBlur(roi, 5)
         letter_rects.append(roi)
 
-    return letter_rects, whitespace
+    distances_sorted = sorted(distances, key=lambda t: t[0], reverse=True)
+    if len(distances_sorted) >= 2:
+        whitespaces = [distances_sorted[0][1], distances_sorted[1][1]]
+    return letter_rects, whitespaces
 
 def recognize_plate(img, coords, model):
     img_height, img_width = img.shape[:2]
     # separate coordinates from box
     xmin, ymin, xmax, ymax = coords
     # get the subimage that makes up the bounded region and take an additional 5 pixels on each side
-    box = img[max(int(ymin) - 5, 0):min(int(ymax) + 5, img_height), max(int(xmin) - 5, 0):min(int(xmax) + 5, img_width)]
+    width_tol = int(img_width * 0.011)
+    box = img[max(int(ymin) - 5, 0):min(int(ymax) + 5, img_height), max(int(xmin) - 0, 0):min(int(xmax) + width_tol, img_width), :]
+    # set blue channel to zero to ignore blue field with euro stars and D
+    box[:,:,0] = 0
     box_height, box_width = box.shape[:2]
     # check if boxes has correct aspect ratios
-    if box_height <= box_width and box_height * 7 >= box_width:
+    if box_height <= box_width and box_height * 10 >= box_width:
         # grayscale region within bounding box
         gray = cv2.cvtColor(box, cv2.COLOR_RGB2GRAY)
         # resize image to three times as large as original for better readability
@@ -97,20 +102,14 @@ def recognize_plate(img, coords, model):
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
         # threshold the image using Otsus method to preprocess
         ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-        # create rectangular kernel for dilation
-        rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-        # apply dilation to make regions more clear
-        dilation = cv2.dilate(thresh, rect_kern, iterations=1)
 
-        letter_rects, whitespace = extract_letters(dilation, gray, thresh)
+        letter_rects, whitespaces = extract_letters(gray, thresh)
 
         keys = get_keys()
 
         firstDigit = False
         license_plate = ''
         for i, letter_rect in enumerate(letter_rects):
-            if i == whitespace:
-                license_plate += ' '
 
             letter_rect = cv2.resize(letter_rect, dsize=(24,40))
             letter_rect = tf.convert_to_tensor(letter_rect, dtype=tf.float32)
@@ -118,6 +117,10 @@ def recognize_plate(img, coords, model):
             letter_rect = letter_rect[tf.newaxis, ..., tf.newaxis]
             key = keys[np.argmax(model.predict(letter_rect))]
             if key.isdigit() and firstDigit == False:
+                if i == whitespaces[0]:
+                    license_plate = license_plate[0:whitespaces[1]] + ' ' + license_plate[whitespaces[1]:]
+                else:
+                    license_plate = license_plate[0:whitespaces[0]] + ' ' + license_plate[whitespaces[0]:]
                 license_plate += ' '
                 firstDigit = True
             license_plate += key
