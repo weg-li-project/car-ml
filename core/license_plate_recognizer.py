536019d824b47f198c929c223d98bfcf5cb97715
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2020-12-22 09:22:21 +0100

    added cnn classification functionality; implemented prototype comparisons

--- core/license_plate_recognizer.py
+++ core/license_plate_recognizer.py
@@ -1,18 +1,21 @@
 
 import cv2
+import numpy as np
+import tensorflow as tf
+
+from core.data_prep import get_keys
 
 def extract_letters(dilation, gray, thresh):
     letter_rects = []
     # find contours of regions of interest within license plate
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # sort contours left-to-right
-    sorted_contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
     # create copy of gray license plate
     im2 = gray.copy()
+    height, width = im2.shape
     # loop through contours and find individual letters and numbers in license plate
-    for cnt in sorted_contours:
-        x,y,w,h = cv2.boundingRect(cnt)
-        height, width = im2.shape
+    keep_cntrs = []
+    for cntr in contours:
+        x,y,w,h = cv2.boundingRect(cntr)
         if h < 20:
             continue
         if h < w:
@@ -26,25 +29,58 @@ def extract_letters(dilation, gray, thresh):
         # if width is not wide enough relative to total width then skip
         if width / float(w) > 15:
             continue
-        # draw the rectangle
-        rect = cv2.rectangle(im2, (x,y), (x+w, y+h), (0,255,0),2)
+        keep_cntrs.append(cntr)
+
+    contours = keep_cntrs
+    keep_cntrs = []
+
+    # sort contours by height and get prototype as third biggest cnt
+    if len(contours) < 3:
+        return [], -1
+
+    prototype = sorted(contours, key=lambda cntr: cv2.boundingRect(cntr)[3], reverse=True)[2]
+
+    for cntr in contours:
+        x, y, w, h = cv2.boundingRect(cntr)
+        _, _, prototype_width, prototype_height = cv2.boundingRect(prototype)
+        if h >= prototype_height * 1.3:
+            continue
+        if h <= prototype_height * 0.7:
+            continue
+        keep_cntrs.append(cntr)
+
+    contours = keep_cntrs
+
+    # sort contours left-to-right
+    contours = sorted(contours, key=lambda cntr: cv2.boundingRect(cntr)[0]) # TODO: sort from upper left to lower right instead
+
+    whitespace = -1
+
+    for i, cntr in enumerate(contours):
+
+        x, y, w, h = cv2.boundingRect(cntr)
+        if i == 0:
+            dist, dist_max = 0, 0
+            urc = x + w
+        else:
+            dist = x - urc
+            urc = x + w
+
+        if dist > dist_max:
+            dist_max = dist
+            whitespace = i
+
         # grab character region of image
-        roi = thresh[max(y-5-int(h*0.13), 0):min(y+h+5, height), max(x-5, 0):min(x+w+5, width)] # some margin for error for umlauts
+        roi = thresh[max(y - 5 - int(h * 0.13), 0):min(y + h + 5, height), max(x - 5, 0):min(x + w + 5, width)]  # some margin for error for umlauts
         # perform bitwise not to flip image to black text on white background
         roi = cv2.bitwise_not(roi)
         # perform another blur on character region
         roi = cv2.medianBlur(roi, 5)
         letter_rects.append(roi)
 
+    return letter_rects, whitespace
 
-    # TODO: find third biggest to build prototype --> loop over all and throw away what is less than 70% and greater than 130% height
-    prototype = sorted(key=lambda ctr: cv2.boundingRect(ctr)[3], reverse=True)[2]
-
-
-    # import matplotlib.pyplot as plt; plt.imshow(roi); plt.show()
-    return letter_rects
-
-def recognize_plate(img, coords):
+def recognize_plate(img, coords, model):
     img_height, img_width = img.shape[:2]
     # separate coordinates from box
     xmin, ymin, xmax, ymax = coords
@@ -65,9 +101,25 @@ def recognize_plate(img, coords):
         rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         # apply dilation to make regions more clear
         dilation = cv2.dilate(thresh, rect_kern, iterations=1)
-        # extract letters from lp box
-        letter_rects = extract_letters(dilation, gray, thresh)
 
-        # TODO: apply cnn on letter_rects with some additional logic to get text
+        letter_rects, whitespace = extract_letters(dilation, gray, thresh)
 
-        # return plate_num
+        keys = get_keys()
+
+        firstDigit = False
+        license_plate = ''
+        for i, letter_rect in enumerate(letter_rects):
+            if i == whitespace:
+                license_plate += ' '
+
+            letter_rect = cv2.resize(letter_rect, dsize=(24,40))
+            letter_rect = tf.convert_to_tensor(letter_rect, dtype=tf.float32)
+            letter_rect = letter_rect / 255.0
+            letter_rect = letter_rect[tf.newaxis, ..., tf.newaxis]
+            key = keys[np.argmax(model.predict(letter_rect))]
+            if key.isdigit() and firstDigit == False:
+                license_plate += ' '
+                firstDigit = True
+            license_plate += key
+
+        return license_plate
