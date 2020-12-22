ca4b6c769be26221b14fed5d89301c3da45207ca
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2020-12-22 08:13:42 +0100

    started implementation of recognize_plate method

---
+++ core/license_plate_recognizer.py
@@ -0,0 +1,73 @@
+
+import cv2
+
+def extract_letters(dilation, gray, thresh):
+    letter_rects = []
+    # find contours of regions of interest within license plate
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # sort contours left-to-right
+    sorted_contours = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[0])
+    # create copy of gray license plate
+    im2 = gray.copy()
+    # loop through contours and find individual letters and numbers in license plate
+    for cnt in sorted_contours:
+        x,y,w,h = cv2.boundingRect(cnt)
+        height, width = im2.shape
+        if h < 20:
+            continue
+        if h < w:
+            continue
+        # if height of box is not tall enough relative to total height then skip
+        if h < 0.2 * height:
+            continue
+        # if height to width ratio is less than 25% skip
+        if w < h * 0.25:
+            continue
+        # if width is not wide enough relative to total width then skip
+        if width / float(w) > 15:
+            continue
+        # draw the rectangle
+        rect = cv2.rectangle(im2, (x,y), (x+w, y+h), (0,255,0),2)
+        # grab character region of image
+        roi = thresh[max(y-5-int(h*0.13), 0):min(y+h+5, height), max(x-5, 0):min(x+w+5, width)] # some margin for error for umlauts
+        # perform bitwise not to flip image to black text on white background
+        roi = cv2.bitwise_not(roi)
+        # perform another blur on character region
+        roi = cv2.medianBlur(roi, 5)
+        letter_rects.append(roi)
+
+
+    # TODO: find third biggest to build prototype --> loop over all and throw away what is less than 70% and greater than 130% height
+    prototype = sorted(key=lambda ctr: cv2.boundingRect(ctr)[3], reverse=True)[2]
+
+
+    # import matplotlib.pyplot as plt; plt.imshow(roi); plt.show()
+    return letter_rects
+
+def recognize_plate(img, coords):
+    img_height, img_width = img.shape[:2]
+    # separate coordinates from box
+    xmin, ymin, xmax, ymax = coords
+    # get the subimage that makes up the bounded region and take an additional 5 pixels on each side
+    box = img[max(int(ymin) - 5, 0):min(int(ymax) + 5, img_height), max(int(xmin) - 5, 0):min(int(xmax) + 5, img_width)]
+    box_height, box_width = box.shape[:2]
+    # check if boxes has correct aspect ratios
+    if box_height <= box_width and box_height * 7 >= box_width:
+        # grayscale region within bounding box
+        gray = cv2.cvtColor(box, cv2.COLOR_RGB2GRAY)
+        # resize image to three times as large as original for better readability
+        gray = cv2.resize(gray, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+        # perform gaussian blur to smoothen image
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        # threshold the image using Otsus method to preprocess
+        ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+        # create rectangular kernel for dilation
+        rect_kern = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        # apply dilation to make regions more clear
+        dilation = cv2.dilate(thresh, rect_kern, iterations=1)
+        # extract letters from lp box
+        letter_rects = extract_letters(dilation, gray, thresh)
+
+        # TODO: apply cnn on letter_rects with some additional logic to get text
+
+        # return plate_num
