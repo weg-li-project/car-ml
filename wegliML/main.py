38b50f5798ec9a50d316a524efa7bd56ead1aef0
tritter1612 <12200682+tritter1612@users.noreply.github.com>
2020-12-29 10:43:36 +0100

    modified get_license_plate_number_suggestions method to make use of alpr_yolo_cnn result first; if no correct license plate was found using alpr_yolo_cnn result the alpr_gcloud_vision result is used

--- wegliML/main.py
+++ wegliML/main.py
@@ -1,14 +1,16 @@
 from typing import List, Final
 
+import numpy as np
 from flask import Request
 from werkzeug.exceptions import UnprocessableEntity, BadRequest, MethodNotAllowed, UnsupportedMediaType
 
-from core.transforms import to_json_suggestions
-from core.annotations import get_annotations_from_gcs_uris
-from alpr.license_plate_recognition import recognize_license_plate
+from alpr_gcloud_vision.core.transforms import to_json_suggestions
+from alpr_gcloud_vision.core.annotations import get_annotations_from_gcs_uris
+from alpr_gcloud_vision.alpr.license_plate_recognition import recognize_license_plate
+from alpr_yolo_cnn.detect import main as alpr_yolo_cnn_main
+from alpr_gcloud_vision.alpr.license_plate_candidate import LicensePlateCandidate
 
 PROP_NAME: Final = 'google_cloud_urls'
-
 
 def get_image_analysis_suggestions(request: Request):
     """Responds to POST HTTP request containing a JSON body with Google Cloud Storage URLs.
@@ -37,10 +39,26 @@ def get_image_analysis_suggestions(request: Request):
 
 def get_license_plate_number_suggestions(google_cloud_urls):
     annotation_data = get_annotations_from_gcs_uris(google_cloud_urls)
-    license_plate_numbers = []
-    for image, object_annotations, text_annotations in annotation_data:
-        for license_plate_number in recognize_license_plate(image, object_annotations, text_annotations):
-            license_plate_numbers.append(license_plate_number)
-    license_plate_number_set = set()
-    uniques = [x for x in license_plate_numbers if x not in license_plate_number_set and (license_plate_number_set.add(x) or True)]
-    return uniques
+    images = [data[0] for data in annotation_data]
+    plate_numbers_dict = alpr_yolo_cnn_main(images, cnn_advanced=False, yolo_checkpoint='./alpr_gcloud_vision/checkpoints/yolov4', cnn_checkpoint='./alpr_yolo_cnn/checkpoints/cnn/training')
+
+    # check if any found license plate number is a valid license plate
+    for key in plate_numbers_dict.keys():
+        results = []
+        for lp in plate_numbers_dict[key]:
+            lpc = LicensePlateCandidate(lp)
+            license_plate_no, res, msg = lpc.checkCandidate()
+            results.append(res)
+
+    if plate_numbers_dict[key] is None or plate_numbers_dict[key] == [] or not (np.array(results)).any():
+        img_path = key
+        for image, object_annotations, text_annotations in annotation_data:
+            if image == img_path:
+                license_plate_nos = recognize_license_plate(image, object_annotations, text_annotations)
+                plate_numbers_dict[key] = license_plate_nos
+
+    # remove duplicates
+    license_plate_numbers_set = {}
+    for l in plate_numbers_dict.values():
+        license_plate_numbers_set = license_plate_numbers_set.union(set(l))
+    return license_plate_numbers_set
