9d672fd1ff611c6057faef63c167563d8df72067
Jacob Rödel <roedel@campus.tu-berlin.de>
2021-01-11 13:17:46 +0100

    Refactor alpr_adapter
    * Add more typehints
    * Reuse loaded image
    * Fix bug

--- wegliML/alpr_gcloud_vision/core/annotations.py
+++ wegliML/alpr_gcloud_vision/core/annotations.py
@@ -1,6 +1,5 @@
-import io
 import os
-from typing import List, Final
+from typing import List, Final, Tuple
 
 from google.cloud import vision
 from google.cloud.vision_v1 import AnnotateImageResponse
@@ -9,7 +8,7 @@ from google.cloud import storage
 BUCKET_NAME: Final = os.environ["WEGLI_IMAGES_BUCKET_NAME"]
 
 
-def get_image_from_gcs_uri(uri: str):
+def get_image_from_gcs_uri(uri: str) -> bytes:
     storage_client = storage.Client()
 
     blob: storage.blob.Blob = storage_client.bucket(BUCKET_NAME) \
@@ -19,33 +18,25 @@ def get_image_from_gcs_uri(uri: str):
     return image_in_bytes
 
 
-def get_annotations_from_gcs_uri(uri: str):
+def get_annotations_from_gcs_uri(image: bytes):
     client = vision.ImageAnnotatorClient()
-    image_in_bytes = get_image_from_gcs_uri(uri)
 
     features = [
         {'type_': vision.Feature.Type.OBJECT_LOCALIZATION},
         {'type_': vision.Feature.Type.TEXT_DETECTION}
     ]
-    image = vision.Image(content=image_in_bytes)
+    image = vision.Image(content=image)
 
     response: AnnotateImageResponse = client.annotate_image({
         'image': image,
         'features': features
     })
 
-    return uri, io.BytesIO(image_in_bytes), response.localized_object_annotations, response.text_annotations
+    return response.localized_object_annotations, response.text_annotations
 
 
-def get_annotations_from_gcs_uris(uris: List[str]):
-    annotation_data = []
-    for uri in uris:
-        annotation_data.append(get_annotations_from_gcs_uri(uri))
-    return annotation_data
-
-
-def get_images_from_gcs_uris(uris: List[str]):
+def get_images_from_gcs_uris(uris: List[str]) -> List[Tuple[str, bytes]]:
     images = []
     for uri in uris:
-        images.append((uri, io.BytesIO(get_image_from_gcs_uri(uri))))
+        images.append((uri, get_image_from_gcs_uri(uri)))
     return images
--- wegliML/util/alpr_adapter.py
+++ wegliML/util/alpr_adapter.py
@@ -1,6 +1,5 @@
-from typing import List
-
-import numpy as np
+import io
+from typing import List, Tuple
 
 from alpr_gcloud_vision.core.annotations import get_annotations_from_gcs_uri
 from util.transforms import order_by_frequency, get_uniques
@@ -10,7 +9,7 @@ from alpr_gcloud_vision.alpr.license_plate_recognition import recognize_license_
 from alpr_yolo_cnn.detect import main as alpr_yolo_cnn_main
 
 
-def recognize_license_plate_numbers(image_data) -> List[str]:
+def recognize_license_plate_numbers(image_data: List[Tuple[str, bytes]]) -> List[str]:
     uris = [data[0] for data in image_data]
     images = [data[1] for data in image_data]
     plate_numbers_dict = alpr_yolo_cnn_main(uris=uris, images=images, cnn_advanced=False,
@@ -25,12 +24,12 @@ def recognize_license_plate_numbers(image_data) -> List[str]:
             license_plate_no, res, msg = lpc.checkCandidate()
             results.append(res)
 
-        if plate_numbers_dict[key] is None or plate_numbers_dict[key] == [] or not (np.array(results)).any():
+        if plate_numbers_dict[key] is None or plate_numbers_dict[key] == []:
             img_path = key
-            for uri, image, object_annotations, text_annotations in get_annotations_from_gcs_uri(img_path):
-                if uri == img_path:
-                    license_plate_nos = recognize_license_plate(image, object_annotations, text_annotations)
-                    plate_numbers_dict[key] = license_plate_nos
+            image = images[uris.index(img_path)]
+            object_annotations, text_annotations = get_annotations_from_gcs_uri(image)
+            license_plate_nos = recognize_license_plate(io.BytesIO(image), object_annotations, text_annotations)
+            plate_numbers_dict[key] = license_plate_nos
 
     license_plate_numbers = [lpn for lpns in plate_numbers_dict.values() for lpn in lpns]
     return get_uniques(order_by_frequency(license_plate_numbers))
