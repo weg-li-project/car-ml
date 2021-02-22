8632cb8af489a0538712c7b59629e24fb16eb7b1
Jacob Rödel <roedel@campus.tu-berlin.de>
2021-02-22 21:28:04 +0100

    Add load test locust file

---
+++ tests/locustfile.py
@@ -0,0 +1,61 @@
+import os
+from typing import Final
+
+from locust import HttpUser, task, tag
+
+IMAGES: Final = {
+    "small": [
+        f"gs://{os.environ['WEGLI_IMAGES_BUCKET_NAME']}/test/IMG_20191027_102538_size_small_car_two.jpg",
+        f"gs://{os.environ['WEGLI_IMAGES_BUCKET_NAME']}/test/IMG_20190811_095743_size_small_car_three.jpg",
+        f"gs://{os.environ['WEGLI_IMAGES_BUCKET_NAME']}/test/IMG_20190811_095734_size_small_car_four.jpg"
+    ],
+    "big": [
+        f"gs://{os.environ['WEGLI_IMAGES_BUCKET_NAME']}/test/IMG_20191129_085112_size_big_car_one.jpg",
+        f"gs://{os.environ['WEGLI_IMAGES_BUCKET_NAME']}/test/IMG_20201030_150403_size_big_car_three.jpg",
+        f"gs://{os.environ['WEGLI_IMAGES_BUCKET_NAME']}/test/IMG_20201026_083127_size_big_car_multiple.jpg"
+    ]
+}
+
+
+class ImageAnalysisUser(HttpUser):
+    @tag('small', 'one')
+    @task(2)
+    def one_small_image(self):
+        self.client.headers.setdefault("Content-Type", "application/json")
+        body = {"google_cloud_urls": [IMAGES["small"][0]]}
+        self.client.post("/", json=body)
+
+    @tag('big', 'one')
+    @task(2)
+    def one_big_image(self):
+        self.client.headers.setdefault("Content-Type", "application/json")
+        body = {"google_cloud_urls": [IMAGES["big"][0]]}
+        self.client.post("/", json=body)
+
+    @tag('small', 'two')
+    @task(4)
+    def two_small_images(self):
+        self.client.headers.setdefault("Content-Type", "application/json")
+        body = {"google_cloud_urls": [*IMAGES["small"][0:1]]}
+        self.client.post("/", json=body)
+
+    @tag('big', 'two')
+    @task(4)
+    def two_big_images(self):
+        self.client.headers.setdefault("Content-Type", "application/json")
+        body = {"google_cloud_urls": [*IMAGES["big"][0:1]]}
+        self.client.post("/", json=body)
+
+    @tag('small', 'three')
+    @task(1)
+    def three_small_images(self):
+        self.client.headers.setdefault("Content-Type", "application/json")
+        body = {"google_cloud_urls": [*IMAGES["small"]]}
+        self.client.post("/", json=body)
+
+    @tag('big', 'three')
+    @task(1)
+    def three_big_images(self):
+        self.client.headers.setdefault("Content-Type", "application/json")
+        body = {"google_cloud_urls": [*IMAGES["big"]]}
+        self.client.post("/", json=body)
